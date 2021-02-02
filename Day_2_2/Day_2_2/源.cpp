454778a230195002915d172e119d3878e8ed4696
Hidden-Forest <376695558@qq.com>
2021-02-02 22:49:09 +0800

    code

---
+++ Day_2_2/Day_2_2/源.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+using namespace std;
+class Solution {
+public:
+	int characterReplacement(string s, int k) {
+
+	}
+};
+int main(){
+	int n;
+	while (cin >> n){
+		if (n==)
+	}
+}
